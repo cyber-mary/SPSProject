87dfc6ec2e1bd466c7ed5a6326eac7aa8124296b
Gerardo Granados <gerardogr.al@outlook.com>
2021-04-03 05:23:42 +0000

    Untrack target and add necessary files

---
+++ src/main/java/sps/team48/classes/Family.java
@@ -0,0 +1,12 @@
+package sps.team48.classes;
+
+// A Family made up of different User Accounts
+public final class Family {
+	private final long id;
+	private final String name;
+
+	public Family(long id, String name) {
+    this.id = id;
+    this.name = name;
+	}
+}
